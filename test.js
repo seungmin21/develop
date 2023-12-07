0ce1e729593f7dd474c4eb6ffcc11279b6c0611e
ohseungmin <ohmin2886@gmail.com>
2023-12-07 15:53:55 +0900

    타입이 object 문자열인지 확인할 수 있는 함수 그리고 if문에 console.log이 없어도 작동에 문제는 없다.

--- test.js
+++ test.js
@@ -14,3 +14,16 @@ if(typeof(example) === "object") {
   console.log("example은 object 문자열이 아닙니다.")
 }
 
+function typeB(object) {
+  if(typeof(object) === "object") {
+    const keyList = Object.keys(example)
+    // 콘솔을 안찍어도 참일때 리턴 값을 확인할 수 있다.
+    //console.log(keyList)
+    return `이 매개변수는 객체입니다.`
+  } else {
+    return "이 매개변수는 객체가 아닙니다."
+  }
+}
+
+let Inspect = typeB(example);
+console.log(Inspect)
