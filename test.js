da14ec968dc4aae659c114e18069e42b14f6de22
ohseungmin <ohmin2886@gmail.com>
2023-12-07 15:42:34 +0900

    Object.keys 메서드를 if문에 사용 조건은 타입검사를한 변수가  object 문자열일때 참

--- test.js
+++ test.js
@@ -7,3 +7,10 @@ let example = {
 const objectList = Object.keys(example)
 console.log(objectList) // [ 'first', 'second', 'third' ]
 
+if(typeof(example) === "object") {
+  const list = Object.keys(example)
+  console.log(list) // [ 'first', 'second', 'third' ]
+} else {
+  console.log("example은 object 문자열이 아닙니다.")
+}
+
